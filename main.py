80e1a831a8f2ffb156d617a5ff97f2eaf077c2e1
albalbero <lovato.albalbero@gmail.com>
2026-07-04 15:36:51 +0200

    Aggiornato main.py così che contenga anche la funzione di dire dov'è il gatto (check_cat() di direction.py)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Questo è il file principale da eseguire per fare funzionare il programma.
 Chiama le funzioni dagli altri file e le esegue
 """
-import camera, yolo, show_result
+import camera, yolo, show_result, direction
 
 if __name__ == "__main__":
     canale = camera.initialize_camera()
@@ -12,6 +12,7 @@ if __name__ == "__main__":
         results = yolo.get_results(model, frame)
         everything = yolo.sort_results(model, results)
         new_frame = show_result.draw_rectangles_and_names(frame, everything)
+        new_frame, is_cat = direction.check_cat(frame, everything)
         camera.output_camera(new_frame)
 
         if camera.quit(key_to_stop="q"):
