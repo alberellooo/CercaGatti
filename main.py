33a47aebd22e99b11616d78301055d265bb0a6d0
albalbero <lovato.albalbero@gmail.com>
2026-07-06 17:20:04 +0200

    Ridotto il controllo di YOLOogni 10 frame così da ottimizzare codice

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ import camera, yolo, show_result, direction
 if __name__ == "__main__":
     canale = camera.initialize_camera()
     model = yolo.initialize_YOLO()
+    count = 0
     while True:
         check, frame = camera.read_frame(canale)
-        results = yolo.get_results(model, frame)
-        everything = yolo.sort_results(model, results)
-        new_frame = show_result.draw_rectangles_and_names(frame, everything)
-        new_frame, is_cat = direction.check_cat(frame, everything)
-        camera.output_camera(new_frame)
+        if count % 10 == 0:
+            results = yolo.get_results(model, frame)
+            everything = yolo.sort_results(model, results)
+            frame, is_cat = direction.check_cat(frame, everything)
+            print(is_cat)
+            count = 0
+        frame = show_result.draw_rectangles_and_names(frame, everything)
+        count += 1
+        camera.output_camera(frame)
 
         if camera.quit(key_to_stop="q"):
             break
